3147118fe7f684e1a1e42ef7c5e5ac9a974f7661
unknown <sebastesb1986@gmail.com>
2022-02-09 00:55:30 -0500

    actualizaciones para aoa-prueba-frontend

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/aoa-prueba-frontend'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/aoa-prueba-frontend/index.html'));
+app.use(express.static(__dirname+'/dist/frontend-aoa'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/frontend-aoa/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
